a36e72615e3485faf5b6d1e43dad9cc3b67c656a
sh <shhang.2021@smu.edu.sg>
2025-10-17 13:22:26 +0800

    Initial Commit

---
+++ Rapper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rapper
+//
+//  Created by Sing Hui Hang on 17/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
